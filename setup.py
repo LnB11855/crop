bedf4f6e7cce81c91ff3c37b36afaac2e70462da
LnB11855 <33381018+LnB11855@users.noreply.github.com>
2018-01-24 21:31:10 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-  setup(name='trainer', packages=['trainer'])
+  setup(name='trainer',
+  packages = find_packages(),
+  install_requires=[
+      'keras',
+      'h5py',
+    'matplotlib.pyplot'
+  ],
+  zip_safe=False
+        )
