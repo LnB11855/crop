d9b080aa70cd2e43364c72c836975d31e66080fa
LnB11855 <33381018+LnB11855@users.noreply.github.com>
2018-01-27 23:46:18 -0600

    Update tensorboard.py

--- trainer/tensorboard.py
+++ trainer/tensorboard.py
@@ -129,11 +129,13 @@ def nn_model(log, X_train, Y_train, XX_val,YY_val,num_epochs = 10000, learning_r
             (minibatch_X, minibatch_Y) = minibatch
             _, minibatch_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
             epoch_cost += minibatch_cost / num_minibatches
-        
+            
         if print_cost and epoch % 1 == 0:
             coff=np.corrcoef(sess.run(Z3, feed_dict={X: X_train, Y: Y_train}), Y_train)[0, 1]
             print ("Cost after epoch %i: %f correlation coefficient: %f" %(epoch, epoch_cost,coff)
-            writer.add_summary(coff, epoch)
+            _train_cost_summary=sess.run([train_cost_summary], 
+                        feed_dict={x: X_train, y_: Y_train})
+            writer.add_summary(_train_cost_summary, epoch)
         costs.append(epoch_cost)
         
       val_cost=0
